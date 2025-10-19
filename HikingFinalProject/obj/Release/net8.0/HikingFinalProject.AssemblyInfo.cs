a687531d4a2073ef37ed4536f996a3897b12210a
sreynolds09 <sara.reynolds.clan@gmail.com>
2025-10-18 20:17:19 -0400

    ignore

--- HikingFinalProject/obj/Release/net8.0/HikingFinalProject.AssemblyInfo.cs
+++ HikingFinalProject/obj/Release/net8.0/HikingFinalProject.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("HikingFinalProject")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b621307048678f701c199a2aa61bebbcc061bde")]
 [assembly: System.Reflection.AssemblyProductAttribute("HikingFinalProject")]
